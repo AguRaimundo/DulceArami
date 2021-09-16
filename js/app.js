efc5b270f758975311ad636008e8a3a06032e0a0
AguRaimundo <raimundoagustin98@gmail.com>
2021-09-16 16:34:02 -0300

    Nuevo html para js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-console.log("ggwp");
+
+let nombre = "Agustin";
+let apellido = "Raimundo";
+let edad = "23";
+
+let mensaje = prompt ("ingrese su nombre");
+let mensaje2 = prompt ("ingrese su apellido");
+let mensaje3 = prompt ("ingrese su edad");
+
+let numero = 10
+let numero1 = 5
+let numero2 = 40
+
+
+console.log("ggwp entraste");
+console.log(numero1 + numero2);
 
