5a951c3529aeb5850269ccaf8fc5e7c204b81113
AguRaimundo <raimundoagustin98@gmail.com>
2021-09-27 11:19:27 -0300

    ej 3

--- js/app.js
+++ js/app.js
@@ -1,28 +1,14 @@
-alert("Bienvenido al programa 1")
-let nombre = prompt ("ingrese su nombre");
-let apellido = prompt ("ingrese su apellido");
-let edad = 23
-let dato = Number(prompt("Ingrese su edad"));
-let sexo = prompt("ingrese sexo");
-let dinero =Number(prompt("Ingrese su Dinero"));
 
-do {('dinero === 15')
-    alert("Felicidades, con 15 pe te haces alto guiso");
+const pass = 1998
+let dato = Number(prompt("Ingrese su Contrasenia"));
+let pruebas = 3
+
+while (dato != pass && pruebas > 0){
+    alert(`Contrasenia Incorrecta, te quedan ${pruebas} intentos`);
+    pruebas--;
+    dato = Number(prompt("Ingrese su Contrasenia"));
 }
-    
-while (dinero != 15); 
-    alert("Que lastima, con 15 pe te haces alto guiso");
-    dinero =Number(prompt("Ingrese su Dinero"));
-
-
-
-if (sexo === "M") {
-    palabra = "Bienvenido";
-}else if (sexo === "F"){
-    palabra = "Bienvenida";
-}else {
-    palabra = "Bienvenide";
+if(dato === pass){
+    console.log( `Bienvenido`);
 }
 
-
-console.log(`${palabra} ${nombre} ${apellido} tu edad es ${edad}`);
