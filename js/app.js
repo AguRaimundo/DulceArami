cc1047872bd34b5c870a6af0635254b664ec25ef
AguRaimundo <raimundoagustin98@gmail.com>
2021-09-27 18:08:16 -0300

    ej4

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+/* 
 const pass = 1998
 let dato = Number(prompt("Ingrese su Contrasenia"));
 let pruebas = 3
@@ -10,5 +10,35 @@ while (dato != pass && pruebas > 0){
 }
 if(dato === pass){
     console.log( `Bienvenido`);
+} */
+const tortaDulcedeLeche = 2000;
+const tortaChocolate = 1850;
+const tortaMousse = 1256;
+const tortaLemonPie = 1831;
+
+
+
+const compra = () => {
+    let total = 0;
+    while (true){
+        dato = prompt("Ingrese su Torta");
+        switch (dato){
+            case "tdl": 
+                total+=tortaDulcedeLeche;
+                break;
+            case "tc":
+                total+=tortaChocolate;
+                break;
+            case "tm":
+                total+=tortaMousse;
+                break;
+            case "tlp":
+                total+=tortaLemonPie;
+                break; 
+            case "fin":
+                return total;
+        }
+    }
 }
 
+console.log(compra());
