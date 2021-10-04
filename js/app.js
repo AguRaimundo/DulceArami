b02e0f5690f8502b2db1aa020d7414c7f0072437
AguRaimundo <raimundoagustin98@gmail.com>
2021-10-04 20:25:56 -0300

    funcion para array

--- js/app.js
+++ js/app.js
@@ -1,44 +1,37 @@
-/* 
-const pass = 1998
-let dato = Number(prompt("Ingrese su Contrasenia"));
-let pruebas = 3
-
-while (dato != pass && pruebas > 0){
-    alert(`Contrasenia Incorrecta, te quedan ${pruebas} intentos`);
-    pruebas--;
-    dato = Number(prompt("Ingrese su Contrasenia"));
-}
-if(dato === pass){
-    console.log( `Bienvenido`);
-} */
-const tortaDulcedeLeche = 2000;
-const tortaChocolate = 1850;
-const tortaMousse = 1256;
-const tortaLemonPie = 1831;
 
 
+// Desafio complementario ARRAYS
 
-const compra = () => {
-    let total = 0;
-    while (true){
-        dato = prompt("Ingrese su Torta");
-        switch (dato){
-            case "tdl": 
-                total+=tortaDulcedeLeche;
-                break;
-            case "tc":
-                total+=tortaChocolate;
-                break;
-            case "tm":
-                total+=tortaMousse;
-                break;
-            case "tlp":
-                total+=tortaLemonPie;
-                break; 
-            case "fin":
-                return total;
-        }
+class Tortas{
+    constructor(nombre, ingredientes, precio, stock){
+    this.nombre = nombre,
+    this.ingredientes = ingredientes,
+    this.precio = precio,
+    this.stock = stock
     }
-}
 
-console.log(compra());
+    getNombre = () => {return this.nombre}
+    getIngredientes = () => {return this.ingredientes}
+    getPrecio = () => {return this.precio}
+    isAvailable = () => {return this.stock > 0 ? true : false}
+
+};
+const tortaCookie = new Tortas("Torta Cookie",["Nueces", "Chocolate", "Dulce de Leche", "Crema Chantilly"],2500, 5);
+const tortaLemonPie = new Tortas("Lemon Pie",["limon", "Masa Sablee", "Merengue"], 1800, 0 );
+const tortaRogel = new Tortas("Torta Rogel", ["Masa Neutra", "Dulce de Leche", "Merengue"],2000,2 );
+
+let listadoTortas = [];
+
+listadoTortas.push(tortaCookie);
+listadoTortas.push(tortaLemonPie);
+listadoTortas.push(tortaRogel);
+/* console.log(listadoTortas); */
+
+const agregarTortas = () => {
+    let nombre = prompt("Ingrese su Torta");
+
+    let torta = new Tortas(nombre);
+
+    listadoTortas.push(torta);
+}
+agregarTortas();
