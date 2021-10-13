aa0ef734828a52c8f0f6c003fad043ba830a8700
AguRaimundo <raimundoagustin98@gmail.com>
2021-10-13 14:21:51 -0300

    ejercicio 8 DOM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,71 @@
+//Desafio DOM 08
+
+
+//Copipaste
+/* console.log(document);
+console.log(document.body);
+// Entrar por ID  - getElementById()
+console.log(document.getElementById("unico"))
+    //ME TRAE SOLO UN OBJETO HTML
+// Entrar por Clase  - getElementsByClassName()
+console.log(document.getElementsByClassName("clase1"))
+    //ME TRAE UN ARRAY DE OBJETOS HTML
+// Entrar por Etiqueta - getElementsByTagName()
+console.log(document.getElementsByTagName("h1"))
+    //ME TRAE UN ARRAY DE OBJETOS HTML
+console.log(document.getElementsByTagName("p")[3])
+document.getElementsByTagName("p")[3].textContent = "Este es el nuevo texto"
+//querySelector
+console.log(document.querySelector("#unico"))
+console.log(document.querySelector(".clase1"))
+console.log(document.querySelector("p"))
+//Solamente me trae 1 elemento, el 1° que encuentra
+console.log(document.querySelectorAll("p")[3])
+document.getElementById("unico").textContent = " Este es el nuevo contenido"
+    //document.getElementById("unico").style.color = "red"
+//document.getElementById("unico").style.display = "none"
+document.getElementById("unico").setAttribute("class", "truco") */
+
+const listado = document.createElement("h2")
+listado.setAttribute("id", "listadoAgregado")
+listado.textContent = "Lista Nueva"
+document.getElementById("aca").appendChild(listado)
+console.log(listado) 
+
+
+
+class listTortas {
+    constructor({ nombre, precio, stock }) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+const datosTortas = () => {
+    const producto = new listTortas({
+        nombre: document.getElementById("nombre").value,
+        precio: document.getElementById("precio").value,
+        stock: document.getElementById("stock").value
+    })
+    const tortaP = document.createElement("div")
+    const torta = document.createElement("h2")
+    torta.textContent = producto.nombre
+    tortaP.appendChild(torta)
+
+    const torta1 = document.createElement("p")
+    torta1.textContent = producto.precio
+    tortaP.appendChild(torta1)
+
+    const torta2 = document.createElement("p")
+    torta2.textContent = producto.stock
+    tortaP.appendChild(torta2) 
+    document.getElementById("aca").appendChild(tortaP)
+    return producto
+} 
+
+
+
+
 /* 
 const pass = 1998
 let dato = Number(prompt("Ingrese su Contrasenia"));
@@ -11,7 +79,7 @@ while (dato != pass && pruebas > 0){
 if(dato === pass){
     console.log( `Bienvenido`);
 } */
-const tortaDulcedeLeche = 2000;
+/* const tortaDulcedeLeche = 2000;
 const tortaChocolate = 1850;
 const tortaMousse = 1256;
 const tortaLemonPie = 1831;
@@ -41,4 +109,4 @@ const compra = () => {
     }
 }
 
-console.log(compra());
+console.log(compra()); */
