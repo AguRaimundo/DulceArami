e2f472ed91361c102da275b4b3c89d772c2552e4
AguRaimundo <raimundoagustin98@gmail.com>
2021-10-13 14:27:56 -0300

    desafio 08

--- js/app.js
+++ js/app.js
@@ -1,31 +1,5 @@
 //Desafio DOM 08
 
-
-//Copipaste
-/* console.log(document);
-console.log(document.body);
-// Entrar por ID  - getElementById()
-console.log(document.getElementById("unico"))
-    //ME TRAE SOLO UN OBJETO HTML
-// Entrar por Clase  - getElementsByClassName()
-console.log(document.getElementsByClassName("clase1"))
-    //ME TRAE UN ARRAY DE OBJETOS HTML
-// Entrar por Etiqueta - getElementsByTagName()
-console.log(document.getElementsByTagName("h1"))
-    //ME TRAE UN ARRAY DE OBJETOS HTML
-console.log(document.getElementsByTagName("p")[3])
-document.getElementsByTagName("p")[3].textContent = "Este es el nuevo texto"
-//querySelector
-console.log(document.querySelector("#unico"))
-console.log(document.querySelector(".clase1"))
-console.log(document.querySelector("p"))
-//Solamente me trae 1 elemento, el 1° que encuentra
-console.log(document.querySelectorAll("p")[3])
-document.getElementById("unico").textContent = " Este es el nuevo contenido"
-    //document.getElementById("unico").style.color = "red"
-//document.getElementById("unico").style.display = "none"
-document.getElementById("unico").setAttribute("class", "truco") */
-
 const listado = document.createElement("h2")
 listado.setAttribute("id", "listadoAgregado")
 listado.textContent = "Lista Nueva"
