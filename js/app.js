f6ff481cfa3e948b7d6f19c1254c957d7ec043a1
AguRaimundo <raimundoagustin98@gmail.com>
2021-10-16 19:38:21 -0300

    ejercicios c9

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 
-// 1ra PREENTREGA
-
-// Simulador del funcionamiento de mi carrito, con un switch donde podes agregar y/o retirar tortas y cuente el total
 
 class Tortas{
-    constructor(nombre, ingredientes, precio, stock, id){
+    constructor(nombre, descripcion, img, ingredientes, precio, stock, id){
     this.nombre = nombre,
+    this.descripcion = descripcion,
+    this.img = img,
     this.ingredientes = ingredientes,
     this.precio = precio,
     this.stock = stock,
@@ -17,13 +16,32 @@ class Tortas{
     getPrecio = () => {return this.precio}
     isAvailable = () => {return this.stock > 0 ? true : false}
     };
-const tortaBrownie = new Tortas("Torta Brownie",["Manteca", "Huevos", "Frutos Rojos", "Crema Chantilly"],2500,2,1);
-const tortaMousse = new Tortas("Mousse de Frutilla",["Crema de Limon", "Huevos", "Esencia de Vainilla"],2300,6,2);
-const tortaFrutosRojos = new Tortas ("Cheescake de Frutos Rojos",["Queso crema","Huevos","Manteca Fundida"],2200,4,3);
-const tortaCookie = new Tortas("Torta Cookie",["Nueces", "Chocolate", "Dulce de Leche", "Crema Chantilly"],2500, 5,4);
-const tortaRogel = new Tortas("Torta Rogel", ["Masa Neutra", "Dulce de Leche", "Merengue"],2000,2,5);
-const tortaLemonPie = new Tortas("Lemon Pie",["limon", "Masa Sablee", "Merengue"], 1800, 0,6);
+const tortaBrownie = new Tortas("Torta Brownie",
+                                "Una base húmeda de brownie con nueces, una abundante capa de dulce de leche, una mousse de chocolate semiamargo y para finalizar merengue italiano. ",
+                                "../assets/Brownie.jpg",["Manteca", "Huevos", "Frutos Rojos", "Crema Chantilly"],2500,2,1);
+
+const tortaMousse = new Tortas("Mousse de Frutilla",
+                                "Base de brownie,  una justa capa dulce de leche, mousse de chocolate SEMIAMARGO, y un mousse de frutillas fresco, que sabe resaltar y completar toda esta torta.",
+                                "../assets/tortaMousseFrutillaN.jpg",["Crema de Limon", "Huevos", "Esencia de Vainilla"],2300,6,2);
+
+const tortaFrutosRojos = new Tortas ("Cheescake de Frutos Rojos",
+                                    "es un clásico postre reconocido mundialmente, a base de queso. Su textura suave cortada con frutos rojos y mermelada arriba, son la combinación perfecta para pasar una tarde",
+                                    "../assets/CheescakeFrutosRojos.jpg",["Queso crema","Huevos","Manteca Fundida"],2200,4,3);
+
+const tortaCookie = new Tortas("Torta Cookie",
+                                "Compuesta por tres capas de una masa sablee de galletitas, relleno con dulce de leche y terminado con ganache de chocolate",
+                                "../assets/cookieNuevo.jpeg",["Nueces", "Chocolate", "Dulce de Leche", "Crema Chantilly"],2500, 5,4);
+
+const tortaRogel = new Tortas("Torta Rogel",
+                                "Un clásico argento, 8 capas de masa neutra a base de huevo, con abundantes capas de dulce de leche y terminado con un delicioso merengue italiano", 
+                                "../assets/rogelNuevo.webp",["Masa Neutra", "Dulce de Leche", "Merengue"],2000,2,5);
+
+const tortaLemonPie = new Tortas("Lemon Pie",
+                                "Base de una masa sablee suave, una crema de limón exquisita y un merengue italiano que nunca falla. Un clásico.",
+                                "../assets/lemon-pie.jpg",["limon", "Masa Sablee", "Merengue"], 1800, 0,6);
+
 let baseDatoTorta = [tortaBrownie,tortaMousse,tortaFrutosRojos,tortaCookie,tortaLemonPie,tortaRogel];
+
 
 class Carrito{
     constructor(productos,total){
@@ -61,20 +79,35 @@ if(queHaySt){
     carro = new Carrito([],0);
 }
 
-//Sumar elemento al carrito mediante evento click en btn (ej 9 y preentrega)                
-//input para ingresar cant de comprar (preentrega)   
-//agregar el resto de tortas 
+
+let printHtml = ``
+let lista = document.getElementById("ulDefinitivo")
+for(let i = 0; i < baseDatoTorta.length; i++){
+    printHtml += `
+            <li class="list-group-item">
+                <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media-body order-2 order-lg-1">
+                        <h5 class="mt-0 font-weight-bold mb-2">${baseDatoTorta[i].nombre}</h5>
+                            <p class="font-italic text-muted mb-0 small">⁣${baseDatoTorta[i].descripcion}</p>
+                                <div class="d-flex align-items-center justify-content-between mt-1">
+                                    <h6 class="font-weight-bold my-2">${baseDatoTorta[i].precio}</h6>
+                                </div>
+                    </div><img src="${baseDatoTorta[i].img}" alt="Torta Brownie" width="200" class="ml-lg-5 order-1 order-lg-2">
+                </div>
+                            <div class="btCompra">
+                                <input type="submit" value="Comprar" onclick="sumaCarro(baseDatoTorta[${i}])">
+                            </div>
+            </li>`
+}
+lista.innerHTML = printHtml;
 
 
-let boton = document.getElementById("sumaCarro");
-//hacer funcion sumaCarro para 1 torta
-//esta funcion agrega por cada click 1 torta mousse al carrito
-const sumaCarro = () =>{
-    
-    if(tortaBrownie.isAvailable()){
-        tortaBrownie.stock--;
-        carro.productos.push(tortaBrownie);
-        carro.total += tortaBrownie.getPrecio();
+const sumaCarro = (torta) =>{
+    console.log(torta)
+    if(torta.isAvailable()){
+        torta.stock--;
+        carro.productos.push(torta);
+        carro.total += torta.getPrecio();
         localStorage.setItem("carrito",JSON.stringify(carro));
     }else{
         alert("No hay stock");
@@ -82,31 +115,3 @@ const sumaCarro = () =>{
 }
 console.log("veo el carro", carro);
 
-
-
-
-/* const chequeoTorta = (listadoTortas,torta) =>{
-    let resultado = listadoTortas.find(cake => cake.id === torta.id);
-    return resultado == undefined ? false : true;
-} 
-
-const getTortaIndice = (listadoTortas,torta) => {
-    return listadoTortas.findIndex(cake => cake.id === torta.id);
-} */
-
-/* const defineTorta = (torta) => {
-    switch (torta){
-        case "cookie": 
-                return tortaCookie;
-        case "rogel":
-                return tortaRogel;
-        case "lemonpie":
-                return tortaLemonPie;
-        case "Mousse":
-                return tortaMousse;
-        case "brownie":
-                return tortaBrownie;
-        case "frutosrojos":
-                return tortaFrutosRojos;
-    }
-}; */
